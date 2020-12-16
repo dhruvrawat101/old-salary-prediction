6f7111684c6b536435bf118e80024f022c6026a0
Dhruv Rawat <dhruvrawat18@gmail.com>
2020-12-16 23:27:41 +0530

    added files

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template,request
+import pickle
+import numpy as np
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def gh():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def home():
+    data1 = request.form['a']
+    data2 = request.form['b']
+    data3 = request.form['c']
+    data4 = request.form['d']
+    data5 = request.form['e']
+    data6 = request.form['f']
+    data7 = request.form['g']
+    data8 = request.form['h']
+    data9 = request.form['i']
+    data10 = request.form['j']
+   
+   
+    arr = np.array([[data1, data2, data3, data4,data5,data6,data7,data8,data9,data10]])
+    pred = model.predict(arr)
+    return render_template('after.html', data=pred)
+
+#second way
+#return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
